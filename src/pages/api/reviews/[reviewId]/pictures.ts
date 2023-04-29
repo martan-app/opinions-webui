58ce919ee46de6767c44f2f973c93d6ffca7f5e2
Talisson Trindade <talissonf@gmail.com>
2023-04-29 13:20:08 -0300

    revert: utilizando api em modo edge

--- src/pages/api/reviews/[reviewId]/pictures.ts
+++ src/pages/api/reviews/[reviewId]/pictures.ts
@@ -1,74 +1,32 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import { NextRequest } from "next/server"
-import supabase from "../../../../utils/supabase-client"
+import type { NextApiRequest, NextApiResponse } from "next";
+import supabase from "../../../../utils/supabase-client";
 
-export const config = {
-  runtime: "experimental-edge",
-}
+type Data = any;
 
-export default async function handler(req: NextRequest) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
   if (req.method === "PATCH") {
-    try {
-      const token = req.headers.get("x-token")
-      const storeId = req.headers.get("x-store-id")
-      // verificar se token ta valido
-      const body = await req.json()
-      const { searchParams } = new URL(req.url)
-      const reviewId = searchParams.get("reviewId")
-      // Process a POST request
-      const { error, status, statusText} = await supabase
-        .from("reviews")
-        .update([
-          {
-            pictures: body,
-          },
-        ])
-        .eq("id", reviewId)
-
-      if (status >= 400) {
-        return new Response(JSON.stringify(error), {
-          statusText,
-          status,
-          headers: {
-            "content-type": "application/json",
-          },
-        })
-      }
-
-      return new Response(null, {
-        statusText,
-        status,
-        headers: {
-          "content-type": "application/json",
-        },
-      })
-
-    } catch (error) {
-      return new Response(JSON.stringify(error), {
-        status: 400,
-        headers: {
-          "content-type": "application/json",
-        },
-      })
-    }
-  } else {
-    return new Response(
-      JSON.stringify({
-        status: 405,
-        error_code: 105,
-        message: "Metodo não permitido",
-        user_message: {
-          en_us: "Method not allowed",
-          pt_br: "Metodo não permitido",
-        },
-        more_info: null,
-      }),
+    const { body, query } = req;
+    const { error } = await supabase.from("reviews").update([
       {
-        status: 405,
-        headers: {
-          "content-type": "application/json",
-        },
-      }
-    )
+        pictures: body,
+      },
+    ]).eq("id", query.reviewId)
+
+    return error ? res.status(500).json(error) : res.status(204).send(null);
+  } else {
+    res.status(405).json({
+      status: 405,
+      error_code: 105,
+      message: "Metodo não permitido",
+      user_message: {
+        en_us: "Method not allowed",
+        pt_br: "Metodo não permitido",
+      },
+      more_info: null,
+    });
   }
 }
--- src/pages/api/reviews/[reviewId]/video.ts
+++ src/pages/api/reviews/[reviewId]/video.ts
@@ -1,75 +1,35 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next"
 import supabase from "../../../../utils/supabase-client"
-import { NextRequest } from "next/server"
 
-export const config = {
-  runtime: "experimental-edge",
-}
+type Data = any
 
-export default async function handler(req: NextRequest) {
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
   if (req.method === "PATCH") {
-    try {
-      const token = req.headers.get("x-token")
-      const storeId = req.headers.get("x-store-id")
-      // verificar se token ta valido
-      const body = await req.json()
-      const { searchParams } = new URL(req.url)
-      const reviewId = searchParams.get("reviewId")
-      // Process a POST request
-      const { error, status, statusText } = await supabase
-        .from("reviews")
-        .update([
-          {
-            video_url: body,
-          },
-        ])
-        .eq("id", reviewId)
-
-      if (status >= 400) {
-        return new Response(JSON.stringify(error), {
-          statusText,
-          status,
-          headers: {
-            "content-type": "application/json",
-          },
-        })
-      }
-
-      return new Response(null, {
-        statusText,
-        status,
-        headers: {
-          "content-type": "application/json",
+    const { body, query } = req
+    const { error } = await supabase
+      .from("reviews")
+      .update([
+        {
+          video_url: body,
         },
-      })
+      ])
+      .eq("id", query.reviewId)
 
-    } catch (error) {
-      new Response(JSON.stringify(error), {
-        status: 500,
-        headers: {
-          "content-type": "application/json",
-        },
-      })
-    }
+    return error ? res.status(500).json(error) : res.status(204).send(null)
   } else {
-    return new Response(
-      JSON.stringify({
-        status: 405,
-        error_code: 105,
-        message: "Metodo não permitido",
-        user_message: {
-          en_us: "Method not allowed",
-          pt_br: "Metodo não permitido",
-        },
-        more_info: null,
-      }),
-      {
-        status: 405,
-        headers: {
-          "content-type": "application/json",
-        },
-      }
-    )
+    res.status(405).json({
+      status: 405,
+      error_code: 105,
+      message: "Metodo não permitido",
+      user_message: {
+        en_us: "Method not allowed",
+        pt_br: "Metodo não permitido",
+      },
+      more_info: null,
+    })
   }
 }
