3dd5db6305b980f49a4af48c499ae3cdfd06d141
Talisson Trindade <talissonf@gmail.com>
2023-04-29 13:25:06 -0300

    chore: correcao gratica em alguns textos

--- src/components/UploadImage.tsx
+++ src/components/UploadImage.tsx
@@ -60,11 +60,11 @@ export function UploadImage(props: Partial<Props>) {
                       width="24"
                       height="24"
                       viewBox="0 0 24 24"
-                      stroke-width="2"
+                      strokeWidth="2"
                       stroke="currentColor"
                       fill="none"
-                      stroke-linecap="round"
-                      stroke-linejoin="round"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
                     >
                       <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
                       <path d="M5 12m-1 0a1 1 0 1 0 2 0a1 1 0 1 0 -2 0"></path>
@@ -83,11 +83,11 @@ export function UploadImage(props: Partial<Props>) {
                         width="24"
                         height="24"
                         viewBox="0 0 24 24"
-                        stroke-width="2"
+                        strokeWidth="2"
                         stroke="currentColor"
                         fill="none"
-                        stroke-linecap="round"
-                        stroke-linejoin="round"
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
                       >
                         <path
                           stroke="none"
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,17 @@
-import { Box, Button, Flex, Text, MantineProvider } from "@mantine/core";
-import { verify } from "jsonwebtoken";
-import { useRef } from "react";
-import supabase from "../utils/supabase-client";
-import { Logo } from "../components/Logo";
+import { Box, Button, Flex, Text, MantineProvider } from "@mantine/core"
+import { verify } from "jsonwebtoken"
+import { useEffect, useRef, useState } from "react"
+import supabase from "../utils/supabase-client"
+import { Logo } from "../components/Logo"
 import NotificationsComponent, {
   NotificationsHandle,
-} from "../components/notifications";
-import Products from "../components/Products";
+} from "../components/notifications"
+import Products from "../components/Products"
 
 export async function getServerSideProps(context: any) {
-  const { query } = context;
-  const token = process.env.NOTIFICATION_TOKEN || "";
-  let decodedToken: any = null;
+  const { query } = context
+  const token = process.env.NOTIFICATION_TOKEN || ""
+  let decodedToken: any = null
 
   if (!query.t || !query.notification) {
     return {
@@ -19,18 +19,18 @@ export async function getServerSideProps(context: any) {
         permanent: false,
         destination: "/not-found",
       },
-    };
+    }
   }
 
   try {
-    decodedToken = verify(query.t || "", token);
+    decodedToken = verify(query.t || "", token)
   } catch (error) {
     return {
       redirect: {
         permanent: false,
         destination: "/not-valid?reason=expired",
       },
-    };
+    }
   }
 
   const { data, error: notErr } = await supabase
@@ -48,7 +48,7 @@ export async function getServerSideProps(context: any) {
       reviews
     `
     )
-    .eq("id", query.notification);
+    .eq("id", query.notification)
 
   if (notErr || !data.length) {
     return {
@@ -56,7 +56,7 @@ export async function getServerSideProps(context: any) {
         permanent: false,
         destination: "/not-found",
       },
-    };
+    }
   }
 
   if (decodedToken.store_id !== data[0].store_id || query.t !== data[0].token) {
@@ -65,19 +65,19 @@ export async function getServerSideProps(context: any) {
         permanent: false,
         destination: "/not-valid?reason=storeId,token",
       },
-    };
+    }
   }
 
-  const notificationBody: any = data[0];
+  const notificationBody: any = data[0]
 
   if (notificationBody) {
     const { data: products, error: prodErr } = await supabase
       .from("products")
       .select("id, name, sku, url, pictures, product_id")
-      .in("id", notificationBody.orders.products);
+      .in("id", notificationBody.orders.products)
 
     if (products) {
-      notificationBody.productBody = products;
+      notificationBody.productBody = products
     }
 
     // reviews
@@ -85,10 +85,10 @@ export async function getServerSideProps(context: any) {
       const { data: reviews, error: revErr } = await supabase
         .from("reviews")
         .select("id, product_id, status, rating")
-        .in("id", notificationBody.reviews);
+        .in("id", notificationBody.reviews)
 
       if (reviews && reviews.length > 0) {
-        notificationBody.reviews = reviews;
+        notificationBody.reviews = reviews
       }
     }
   }
@@ -98,90 +98,86 @@ export async function getServerSideProps(context: any) {
       notification: notificationBody,
       decodedToken,
     },
-  };
+  }
 }
 
 export default function Home(props: any) {
-  const { notification } = props;
-  const $alert = useRef<NotificationsHandle>(null);
+  const { notification, decodedToken } = props
+  const $alert = useRef<NotificationsHandle>(null)
+  const [show, __show] = useState(false)
 
   function renderButton() {
     if (!notification?.stores?.url) {
-      return null;
+      return null
     }
 
     return (
       <a target="_blank" href={notification.stores.url} rel="noreferrer">
         <Button>Acessar site</Button>
       </a>
-    );
+    )
+  }
+
+  useEffect(() => {
+    setTimeout(() => {
+      __show(true)
+    }, 100)
+  }, [])
+
+  if (!notification || !decodedToken || !show) {
+    return null
   }
 
   return (
-    <MantineProvider
-      theme={{
-        colors: {
-          brand: [
-            "#DC493A",
-            "#4392F1",
-            "#ECE8EF",
-            "#E3EBFF",
-            "#E7F0FF",
-          ],
-        },
-        primaryColor: "brand",
+    <Box
+      sx={{
+        width: "100%",
       }}
     >
-      <Box
+      <Flex
         sx={{
+          maxWidth: "590px",
           width: "100%",
+          margin: "50px auto",
+          //background: "#fff",
         }}
+        mih={50}
+        gap="xl"
+        justify="flex-start"
+        align="center"
+        direction="column"
+        wrap="wrap"
       >
-        <Flex
-          sx={{
-            maxWidth: "590px",
-            width: "100%",
-            margin: "50px auto",
-            background: "#fff",
-          }}
-          mih={50}
-          gap="xl"
-          justify="flex-start"
-          align="center"
-          direction="column"
-          wrap="wrap"
-        >
-          {notification.stores && notification.stores.logo_url && (
-            <Logo src={notification.stores.logo_url} />
-          )}
+        {notification.stores && notification.stores.logo_url && (
+          <Logo src={notification.stores.logo_url} />
+        )}
 
-          <Text span p="xl" align="center">
-            Sua avaliação é fundamental e pode ajudar outros Clientes a saberem
-            mais sobre a qualidade dos produtos. Obrigado por sua contribuição ;
-          </Text>
+        <Text span p="xl" align="center">
+          Sua avaliação é fundamental e pode ajudar outros Clientes a saberem
+          mais sobre a o produto.
+        </Text>
 
-          <Products
-            products={notification.productBody}
-            notification={notification}
-            alertComponent={$alert.current}
-          />
+        <Products
+          products={notification.productBody}
+          notification={notification}
+          alertComponent={$alert.current}
+        />
 
-          <Text span p="xl" align="center">
-            Você pode cancelar o recebimento dos comunicados relacionados as
-            avaliacoes nos e-mails enviados.
-          </Text>
+        <Text span p="xl" align="center">
+          Você pode cancelar o recebimento dos comunicados relacionados as
+          avaliações nos e-mails enviados.
+        </Text>
 
-          {renderButton()}
+        {renderButton()}
 
-          <Text span>
-            © {new Date().getFullYear()}{" "}
-            {notification.stores && notification.stores.name}. Todos os direitos
-            reservados.
-          </Text>
-        </Flex>
+        <Text span>
+          © {new Date().getFullYear()}{" "}
+          {notification.stores && notification.stores.name}. Todos os direitos
+          reservados.
+        </Text>
+      </Flex>
 
-        <NotificationsComponent ref={$alert} />
-      </Box>
-    </MantineProvider>
-  );
+      <NotificationsComponent ref={$alert} />
+    </Box>
+  )
 }
