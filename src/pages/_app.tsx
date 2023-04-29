04acffd372291fcd51e631296bd5be5812fd9674
Talisson Trindade <talissonf@gmail.com>
2023-04-29 13:40:35 -0300

    build: evitando que build seja quebrado por variavel inexistente

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,7 +8,7 @@ export default function App(props: AppProps) {
   return (
     <>
       <Head>
-        <title>{pageProps.notification.stores.name} - Avaliação de produtos</title>
+        <title>{pageProps?.notification?.stores?.name} - Avaliação de produtos</title>
         <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
       </Head>
 
