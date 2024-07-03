39a84423d28cbd153b0c55ef0e7e166714880fd1
Talisson Trindade <talissonf@gmail.com>
2024-07-03 10:14:04 -0300

    chore: send error logs to better logs

--- src/server-side-props/reviews.ts
+++ src/server-side-props/reviews.ts
@@ -1,112 +1,7 @@
 import { withLogtailGetServerSideProps } from "@logtail/next";
 import { verify } from "jsonwebtoken";
 
-export const getServerSideProps = withLogtailGetServerSideProps(
-  async ({ req, log, query }: any) => {
-    log.info("Hello Logtail!");
-    const token = process.env.NOTIFICATION_TOKEN ?? "";
-    let decodedToken: any = null;
-
-    if (query.t && query.notification) {
-      try {
-        decodedToken = verify(query.t || "", token);
-        const headers = new Headers();
-        headers.append("X-Store-Id", decodedToken.store_id);
-        headers.append("X-Token", query.t);
-        headers.append("Content-Type", "application/json");
-
-        const reqOptions = {
-          method: "get",
-          headers,
-        };
-
-        const urlReviews = new URL(
-          "/v1/notifications/" + query.notification,
-          process.env.CORE_API
-        );
-
-        const req = await fetch(urlReviews.toString(), reqOptions);
-
-        const res: any = await req.json();
-
-        if (!req.ok) {
-          log.error("Opinions-Webui: Notifications Resquest Failed", {
-            erroCode: 1799,
-            req,
-            res,
-          });
-          return {
-            redirect: {
-              permanent: false,
-              destination: "/error?reason=1799",
-            },
-          };
-        }
-
-        if (decodedToken.store_id !== res.notification.store_id) {
-          log.error("Opinions-Webui: storeId diferente from token", {
-            erroCode: 1800,
-            res,
-            decodedToken,
-          });
-          return {
-            redirect: {
-              permanent: false,
-              destination: "/error?reason=1800",
-            },
-          };
-        }
-
-        let notificationBody: any = {
-          store_id: parseInt(decodedToken.store_id, 10),
-          id: query.notification,
-          order_ref: decodedToken.order_id,
-          order_id: decodedToken.order_id,
-        };
-
-        if (res.notification) {
-          if (res?.order?.products) {
-            notificationBody.productBody = res.order.products;
-          }
-
-          if (res?.reviews) {
-            notificationBody.reviews = res.reviews;
-          }
-
-          notificationBody.stores = res.store;
-          notificationBody.customers = res.customer;
-        }
-
-        return {
-          props: {
-            notification: notificationBody,
-            decodedToken,
-          },
-        };
-      } catch (error: any) {
-        log.error("Opinions-Webui: request failed", {
-          erroCode: 1801,
-          error,
-        });
-        return {
-          redirect: {
-            permanent: false,
-            destination: "/error?reason=1801&e=" + JSON.stringify(error),
-          },
-        };
-      }
-    }
-
-    return {
-      redirect: {
-        permanent: false,
-        destination: "/error?reason=1802",
-      },
-    };
-  }
-);
-
-// export async function getServerSideProps(context: any) {
+// function serverProps(context: any) {
 //   const { query } = context;
 //   const token = process.env.NOTIFICATION_TOKEN ?? "";
 //   let decodedToken: any = null;
@@ -208,3 +103,210 @@ export const getServerSideProps = withLogtailGetServerSideProps(
 //     },
 //   };
 // }
+
+// export const getServerSideProps = withLogtailGetServerSideProps(
+//   async ({ log, query }: any) => {
+//     console.log({ log, query });
+//     log.info("Funcionou!");
+//     const token = process.env.NOTIFICATION_TOKEN ?? "";
+//     let decodedToken: any = null;
+
+//     if (query.t && query.notification) {
+//       try {
+//         decodedToken = verify(query.t || "", token);
+//         const headers = new Headers();
+//         headers.append("X-Store-Id", decodedToken.store_id);
+//         headers.append("X-Token", query.t);
+//         headers.append("Content-Type", "application/json");
+
+//         const reqOptions = {
+//           method: "get",
+//           headers,
+//         };
+
+//         const urlReviews = new URL(
+//           "/v1/notifications/" + query.notification,
+//           process.env.CORE_API
+//         );
+
+//         const req = await fetch(urlReviews.toString(), reqOptions);
+//         if (!req.ok) {
+//           log.error("Opinions-Webui: Notifications Resquest Failed", {
+//             erroCode: 1799,
+//             req,
+//           });
+//           return {
+//             redirect: {
+//               permanent: false,
+//               destination: "/error?reason=1799",
+//             },
+//           };
+//         }
+
+//         const res: any = await req.json();
+//         if (decodedToken.store_id !== res.notification.store_id) {
+//           log.error("Opinions-Webui: storeId diferente from token", {
+//             erroCode: 1800,
+//             res,
+//             decodedToken,
+//           });
+//           return {
+//             redirect: {
+//               permanent: false,
+//               destination: "/error?reason=1800",
+//             },
+//           };
+//         }
+
+//         let notificationBody: any = {
+//           store_id: parseInt(decodedToken.store_id, 10),
+//           id: query.notification,
+//           order_ref: decodedToken.order_id,
+//           order_id: decodedToken.order_id,
+//         };
+
+//         if (res.notification) {
+//           if (res?.order?.products) {
+//             notificationBody.productBody = res.order.products;
+//           }
+
+//           if (res?.reviews) {
+//             notificationBody.reviews = res.reviews;
+//           }
+
+//           notificationBody.stores = res.store;
+//           notificationBody.customers = res.customer;
+//         }
+
+//         return {
+//           props: {
+//             notification: notificationBody,
+//             decodedToken,
+//           },
+//         };
+//       } catch (error: any) {
+//         log.error("Opinions-Webui: request failed", {
+//           erroCode: 1801,
+//           error,
+//         });
+
+//         return {
+//           redirect: {
+//             permanent: false,
+//             destination: "/error?reason=1801&e=" + JSON.stringify(error),
+//           },
+//         };
+//       }
+//     }
+
+//     return {
+//       redirect: {
+//         permanent: false,
+//         destination: "/error?reason=1802",
+//       },
+//     };
+//   }
+// );
+
+export async function getServerSideProps(context: any) {
+  const { query } = context;
+  const token = process.env.NOTIFICATION_TOKEN ?? "";
+  let decodedToken: any = null;
+
+  if (query.t && query.notification) {
+    try {
+      decodedToken = verify(query.t || "", token);
+      const headers = new Headers();
+      headers.append("X-Store-Id", decodedToken.store_id);
+      headers.append("X-Token", query.t);
+      headers.append("Content-Type", "application/json");
+
+      const reqOptions = {
+        method: "get",
+        headers,
+      };
+
+      const urlReviews = new URL(
+        "/v1/notifications/" + query.notification,
+        process.env.CORE_API
+      );
+
+      const req = await fetch(urlReviews.toString(), reqOptions);
+
+      const res: any = await req.json();
+
+      if (!req.ok) {
+        console.error("Opinions-Webui: Notifications Resquest Failed", {
+          erroCode: 1799,
+          req,
+          res,
+        });
+        return {
+          redirect: {
+            permanent: false,
+            destination: "/error?reason=1799",
+          },
+        };
+      }
+
+      if (decodedToken.store_id !== res.notification.store_id) {
+        console.error("Opinions-Webui: storeId diferente from token", {
+          erroCode: 1800,
+          res,
+          decodedToken,
+        });
+        return {
+          redirect: {
+            permanent: false,
+            destination: "/error?reason=1800",
+          },
+        };
+      }
+
+      let notificationBody: any = {
+        store_id: parseInt(decodedToken.store_id, 10),
+        id: query.notification,
+        order_ref: decodedToken.order_id,
+        order_id: decodedToken.order_id,
+      };
+
+      if (res.notification) {
+        if (res?.order?.products) {
+          notificationBody.productBody = res.order.products;
+        }
+
+        if (res?.reviews) {
+          notificationBody.reviews = res.reviews;
+        }
+
+        notificationBody.stores = res.store;
+        notificationBody.customers = res.customer;
+      }
+
+      return {
+        props: {
+          notification: notificationBody,
+          decodedToken,
+        },
+      };
+    } catch (error: any) {
+      console.error("Opinions-Webui: request failed", {
+        erroCode: 1801,
+        error,
+      });
+      return {
+        redirect: {
+          permanent: false,
+          destination: "/error?reason=1801&e=" + JSON.stringify(error),
+        },
+      };
+    }
+  }
+
+  return {
+    redirect: {
+      permanent: false,
+      destination: "/error?reason=1802",
+    },
+  };
+}
